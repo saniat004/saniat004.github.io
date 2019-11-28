b834ff891303487d861a7d879011787c74f9d03d
saniat004 <57046529+saniat004@users.noreply.github.com>
2019-11-28 18:22:18 +0100

    Updates

---
+++ precache-manifest.34d41f7eae73bb87db6faa0abe948b89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4c6045bf23187c39e65d81581c291cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e97070d393ad87044474",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "9a91749789cd3b5b0007",
+    "url": "/static/css/main.770c58e9.chunk.css"
+  },
+  {
+    "revision": "e97070d393ad87044474",
+    "url": "/static/js/2.814c5b91.chunk.js"
+  },
+  {
+    "revision": "9a91749789cd3b5b0007",
+    "url": "/static/js/main.e70a667e.chunk.js"
+  },
+  {
+    "revision": "9bf820bd41e6d656f8db",
+    "url": "/static/js/runtime-main.f59df13c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
